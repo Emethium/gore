c2d679e7c08d7d9accc315905c08b734891ac104
emethium <nikolas.serafini@gmail.com>
2018-04-22 14:30:49 -0300

    re-organizing

---
+++ basics_with_tdd/struct/shapes.go
@@ -0,0 +1,9 @@
+package main
+
+func Perimeter(width float64, heigth float64) float64 {
+	return 2 * (width + heigth)
+}
+
+func Area(width float64, heigth float64) float64 {
+	return width * heigth
+}
