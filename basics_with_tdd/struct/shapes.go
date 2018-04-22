c955bd311fc443c3ad2848b03bd149602d3c9d63
emethium <nikolas.serafini@gmail.com>
2018-04-22 16:19:05 -0300

    added struct resources

--- basics_with_tdd/struct/shapes.go
+++ basics_with_tdd/struct/shapes.go
@@ -1,9 +1,42 @@
 package main
 
-func Perimeter(width float64, heigth float64) float64 {
-	return 2 * (width + heigth)
+import (
+	"math"
+)
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width float64, heigth float64) float64 {
-	return width * heigth
+// Rectangle shenanigans
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Circle shenanigans
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Triangle shenanigans
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (c Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
 }
